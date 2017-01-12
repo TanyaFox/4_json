99ee706484147c11a95026ebc85e605a37d63235
TanyaFox <tanya-smile1994@mail.ru>
2017-01-12 23:00:21 +0400

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,27 @@
 import json
-
+import os.path as path
 
 def load_data(filepath):
-    pass
+    if path.exists(filepath):
+        with open(filepath) as file:
+            return json.load(file)
+    else:
+        return None
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        while True:
+            print("Enter your filepath:")
+            jsonpath = input()
+            if jsonpath != None:
+                jsondata = load_data(jsonpath)
+                if jsondata:
+                    pretty_print_json(jsondata)
+                    break
+    except:
+        print("Unknown error occured")
